2da7bb0cd20c312bc3f18fa4f79a80c5971fddbc
GABRIEL  BRAZ <86087849+gbrazdev@users.noreply.github.com>
2022-05-16 13:20:48 -0300

    Add files via upload

---
+++ src/main/java/net/codejava/CREATE_TABLE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `codejavadb`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
